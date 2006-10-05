99d65b952a187f0423e6806351862e3dd4d76052
mbaas <mbaas>
2006-10-05 17:02:33 +0000

    Enhanced the built-in preprocessor. It's still not a full-featured preprocessor but at lesst it now handles line-continuation, simple macros and the most common preprocessor directives.

--- cgkit/simplecpp.py
+++ cgkit/simplecpp.py
@@ -34,114 +34,390 @@
 # ***** END LICENSE BLOCK *****
 # $Id: simplecpp.py,v 1.2 2006/01/27 22:11:09 mbaas Exp $
 
-import types, os.path
+import sys, types, os.path
+
+class Context:
+    """This class carries information about a particular source file.
+    """
+    def __init__(self, filehandle):
+        # The file handle (file-like object)
+        self.filehandle = filehandle
+        # The current line number (i.e. the line number of the next read line)
+        self.linenr = 1
+        # The file name
+        self.filename = getattr(filehandle, "name", "<?>")
+        
+        self.condition_list = []
+        # Flag that specifies whether a line should be ignored or not
+        self.output_line = True
+        
 
 # PreProcessor
 class PreProcessor:
     """A simple 'preprocessor'.
 
     This class implements a subset of the functionality of the C preprocessor.
-
-    So far, only comments are stripped (strings are properly processed, i.e.
-    comment characters inside strings are ignored).
     """
 
     def __init__(self):
         # Flag that specifies if the starting position of the next line is
         # already inside a comment (i.e. if there was a '/*' without a '*/'
         self.inside_comment = False
+        
+        # A 'stack' of Context objects
+        self.context_stack = []
+        # Current context (this is the topmost element from context_stack)
+        self.context = None
+        
+        # Contains the defined macros.
+        self.defined = {}
+        # This is a sorted list containing the macros (longest names first)
+        # The items are tuples (name, value).
+        self.substitution_list = []
+        
+        self.output_buffer = []
 
     def __call__(self, source, errstream=None):
         """Preprocess a file or a file-like object.
 
         source may either be the name of a file or a file-like object.
         """
-        if isinstance(source, types.StringTypes):
-            filename = source
-            # Read the input file
-            f = open(filename)
-            s = f.read()
-            f.close()
-        else:
-            s = source.read()
-            filename = getattr(source, "name", "<?>")
-
-        # Filter
-        s = self.filter(s)
-        # Return the filtered text
-        res = '# 1 "%s"\n%s'%(os.path.basename(filename), s)
-        return res
-
-    # filter
-    def filter(self, s):
-        """Filter a string (which may contain multiple lines).
-
-        s may either be a string or a file-like object.
-        """
-        # If s is no string it must be a file-like object
-        if not isinstance(s, types.StringTypes):
-            s = s.read()
-            
-        reslist = map(lambda x: self.filterLine(x), s.split("\n"))
-        return "\n".join(reslist)
-       
-    # filterLine
-    def filterLine(self, s):
-        """Filter one single line.
-        """
-
-        # Replace string contents with dots...
-        s2 = s
-        offset = 0
-        while 1:
-            # Search the beginning of a string
-            n1 = s.find('"', offset)
-            if n1==-1:
-                break
-            # Search the end of the string (ignore quoted apostrophes)...
-            start = n1+1
-            n2 = None
-            while 1:
-                n2 = s.find('"', start)
-                if n2==-1:
-                    break
-                elif s[n2-1]!='\\':
-                    break
-                else:
-                    start = n2+1
-            if n2==-1:
-                n2 = len(s2)
-            le = n2-n1-1
-            # Replace the string contents with dots
-            s2 = s2[:n1+1] + le*"." + s2[n2:]
-            offset += n2+1
-
-        return self._filterLine(s,s2)
-            
-    def _filterLine(self, s1, s2):
-        """Helper for the filterLine() method.
-        """
-        if self.inside_comment:
-            n = s2.find("*/")
-            if n==-1:
-                return ""
-            n+=2
-            self.inside_comment = False
-            return n*" " + self._filterLine(s1[n:], s2[n:])
         
-        else:            
-            n1 = s2.find("//")
-            n2 = s2.find("/*")
-          
-            # No comment? then return the entire line
-            if n1==-1 and n2==-1:
-                return s1
-            # Only a '//'? Then return the line without that comment
-            elif n1!=-1 and (n2==-1 or n2>n1):                
-                return s1[:n1]
-            # Only a '/*'?
-            elif n2!=-1 and (n1==-1 or n1>n2):
-                pre = s1[:n2]
-                self.inside_comment = True
-                return pre+"  "+self._filterLine(s1[n2+2:], s2[n2+2:])
+        if isinstance(source, types.StringTypes):
+            fhandle = file(source, "rt")
+        else:
+            fhandle = source
+
+        self.output_buffer = []
+        self.preprocess(fhandle)
+        return "\n".join(self.output_buffer)
+
+    def preprocess(self, fhandle):
+        """Preprocess a source file.
+        """
+        
+        self.context_stack = []
+        self.context = None
+        ctx = Context(fhandle)
+        self.readFile(ctx)
+        
+    def readFile(self, ctx):
+        """Read another file.
+        
+        ctx is a initialized Context object.
+        """
+        
+        self.pushContext(ctx)
+        linebuffer = ""
+        defined = self.defined
+        
+        for line in ctx.filehandle:
+            if line[-1:]=='\n':
+                line = line[:-1]
+            # Append the line to the line buffer...
+            if line[-1:]=='\\':
+                linebuffer += line[:-1]
+                # Read another line before processing the line
+                self.context.linenr += 1
+                continue
+            else:
+                linebuffer += line
+                
+            intervals = list(self.iterStringIntervals(linebuffer))
+            out = "".join(intervals)
+                 
+            stripped = out.strip()
+            # Preprocessor directive?
+            if stripped[:1]=="#":
+                s = stripped[1:].strip()
+                a = s.split()
+                directive = a[0].lower()
+                handlername = "on%s"%directive.capitalize()
+                arg = s[len(directive)+1:].strip()
+                handler = getattr(self, handlername, None)
+                if handler!=None:
+                    handler(directive, arg)
+            else:
+                out = ""
+                for s in intervals:
+                    # Do substitutions if it's not a string
+                    if s[0:]!='"':
+                        for name,value in self.substitution_list:
+                            s = s.replace(name, value)
+                    out += s
+
+                if ctx.output_line:
+                    self.output(out)
+                else:
+                    self.output("")
             
+            self.context.linenr += 1
+            linebuffer = ""
+            
+        self.popContext()
+
+    def output(self, s):
+        """This method is called for every output line.
+        """
+        self.output_buffer.append(s)
+            
+    def onInclude(self, directive, arg):
+        """Handle #include directives.
+        """
+        filename = arg[1:-1]
+        if os.path.exists(filename):
+            f = file(filename, "rt")
+            ctx = Context(f)
+            self.readFile(ctx)
+        else:
+            ctx = self.context
+            print >>sys.stderr, "%s:%d: %s: No such file or directory"%(ctx.filename, ctx.linenr, filename)
+        
+    def onDefine(self, directive, arg):
+        """Handle #define directives.
+        """
+        a = arg.split()
+        if len(a)==0:
+            return
+        name = a[0]
+        value = " ".join(a[1:])
+        self.defined[name] = value
+        self.substitution_list.append((name, value))
+        self.substitution_list.sort(cmp=lambda a,b: cmp(len(b[0]), len(a[0])))
+
+    def onUndef(self, directive, arg):
+        """Handle #undef directives.
+        """
+        if arg=="":
+            return
+        if arg not in self.defined:
+            return
+        
+        del self.defined[arg]
+        # Remove the macro from the substitution list
+        lst = []
+        for name,value in self.substitution_list:
+            if name!=arg:
+                lst.append((name,value))
+        self.substitution_list = lst
+        
+    def onIf(self, directive, arg):
+        """Handle #if directives.
+        """
+        try:
+            cond = bool(int(arg))
+        except:
+            cond = False
+        self.context.condition_list.append(cond)
+        self.context.output_line = cond
+        
+    def onIfdef(self, directive, arg):
+        """Handle #ifdef directives.
+        """
+        a = arg.split()
+        if len(a)==0:
+            return
+
+        name = a[0]
+        cond = self.defined.has_key(name)
+        self.context.condition_list.append(cond)
+        self.context.output_line = cond
+
+    def onIfndef(self, directive, arg):
+        """Handle #ifndef directives.
+        """
+        a = arg.split()
+        if len(a)==0:
+            return
+        
+        name = a[0]
+        cond = not self.defined.has_key(name)
+        self.context.condition_list.append(cond)
+        self.context.output_line = cond
+
+    def onElif(self, directive, arg):
+        """Handle #elif directives.
+        """
+        try:
+            cond = bool(int(arg))
+        except:
+            cond = False
+        self.context.output_line = cond
+        
+    def onElse(self, directive, arg):
+        """Handle #else directives.
+        """
+        self.context.output_line = not self.context.output_line
+                
+    def onEndif(self, directive, arg):
+        """Handle #endif directives.
+        """
+        if len(self.context.condition_list)==0:
+            print >>sys.stderr, "%s:%d: unbalanced '#endif'"%(self.context.filename, self.context.linenr)
+            return
+        
+        self.context.condition_list.pop()
+        self.context.output_line = True
+        
+    def onPragma(self, directive, arg):
+        """Handle #pragma directives.
+        """
+        pass
+
+    def pushContext(self, ctx):
+        """Push a context object onto the context stack.
+        """
+        self.context_stack.append(ctx)
+        self.context = ctx
+        self.output('# %d "%s"'%(ctx.linenr, ctx.filename))
+        
+    def popContext(self):
+        """Pop the topmost context object from the context stack.
+        """
+        ctx = self.context_stack.pop()
+        if len(self.context_stack)==0:
+            self.context = None
+        else:
+            self.context = self.context_stack[-1]
+            self.output('# %d "%s"'%(self.context.linenr, self.context.filename))
+            self.output("")
+        return ctx
+
+    def iterStringIntervals(self, s):
+        """Iterate over the separate 'intervals' in the given string.
+        
+        This iterator splits the string into several intervals and yields
+        each interval. The intervals can be one of the following:
+        
+        - Content: Anything outside a comment and outside a string
+        - String: Always begins with '"'
+        - Comment: Is replaced by blanks (or not yielded at all)
+        
+        Example string: 
+        
+          'result = myfunc(5, "text" /* comment */, 6) // Comment'
+          
+        Intervals:
+        
+        - 'result = myfunc(5, '
+        - '"text"'
+        - ' '
+        - '             '
+        - ', 6) '
+        """
+
+        slength = len(s)
+
+        start = 0
+        while start<slength:
+        
+            if self.inside_comment:
+                # Search for the end of the comment
+                n = s.find('*/', start)
+                if n==-1:
+                    start = slength
+                else:
+                    self.inside_comment = False
+                    start = n+2
+            else:
+                n1 = s.find('//', start)
+                n2 = s.find('/*', start)
+                n3 = s.find('"', start)
+
+                # Determine which of the above strings is encountered first...
+                if n1==-1:
+                    if n2==-1:
+                        if n3==-1:
+                            # n1==-1 and n2==-1 and n3==-1
+                            first = 0
+                        else:
+                            # n1==-1 and n2==-1 and n3!=-1
+                            first = 3
+                    else:
+                        if n3==-1:
+                            # n1==-1 and n2!=-1 and n3==-1
+                            first = 2
+                        else:
+                            # n1==-1 and n2!=-1 and n3!=-1
+                            if n2<n3:
+                                first = 2
+                            else:
+                                first = 3
+                else:
+                    if n2==-1:
+                        if n3==-1:
+                            # n1!=-1 and n2==-1 and n3==-1
+                            first = 1
+                        else:
+                            # n1!=-1 and n2==-1 and n3!=-1
+                            if n1<n3:
+                                first = 1
+                            else:
+                                first = 3
+                    else:
+                        if n3==-1:
+                            # n1!=-1 and n2!=-1 and n3==-1
+                            if n1<n2:
+                                first = 1
+                            else:
+                                first = 2
+                        else:
+                            # n1!=-1 and n2!=-1 and n3!=-1
+                            if n1<n2 and n1<n3:
+                                first = 1
+                            else:
+                                if n2<n3:
+                                    first = 2
+                                else:
+                                    first = 3
+
+                # Neither a comment, nor quotes
+                if first==0:
+                    # The entire remaining string is content
+                    yield s[start:]
+                    start = slength
+                # '//' comment
+                elif first==1:
+                    if n1>start:
+                        yield s[start:n1]
+                    start = slength
+                # '/*' comment
+                elif first==2:
+                    if n2>start:
+                        yield s[start:n2]
+                    # Search for the closing comment
+                    n = s.find('*/', n2+2)
+                    if n==-1:
+                        self.inside_comment = True
+                        start = slength
+                    else:
+                        yield (n-n2+2)*" "
+                        start = n+2
+                # '"'
+                else:
+                    if n3>start:
+                        yield s[start:n3]
+                    # Search for the closing apostrophes and yield the string
+                    n = s.find('"', n3+1)
+                    if n==-1:
+                        # No closing apostrophes in the same line? This is
+                        # actually an error
+                        yield s[n3:]
+                        start = slength
+                    else:
+                        n += 1
+                        yield s[n3:n]
+                        start = n
+                
+                        
+                    
+                    
+    
+
+######################################################################
+
+if __name__=="__main__":
+    
+    p = PreProcessor()
+    print p(file(sys.argv[1]))
+    

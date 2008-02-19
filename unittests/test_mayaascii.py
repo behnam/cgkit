482bb514565361fa390758d68b77717bc3bbc458
mbaas <mbaas>
2008-02-19 21:51:04 +0000

    Updates

--- unittests/test_mayaascii.py
+++ unittests/test_mayaascii.py
@@ -3,7 +3,22 @@
 import unittest
 from cgkit import mayaascii
 
+class TestReader(mayaascii.MAReader):
+    def onSetAttr(self, attr, vals, opts):
+#        print attr,vals,opts
+        a = mayaascii.Attribute(attr,vals,opts)
+        #print "  ",a.getValue()
+
 class TestMayaAscii(unittest.TestCase):
+
+    def testDefaultMAReader(self):
+        rd = TestReader()
+        rd.read("data/objects.ma")
+        
+        rd = mayaascii.DefaultMAReader()
+        rd.read("data/objects.ma")
+        t = rd.nodelist[0]
+#        print t.getAttrValue("t", "t", "double3")
 
     def testMAReader_findString(self):
         """Test the MAReader.findString() method.
@@ -32,6 +47,22 @@ class TestMayaAscii(unittest.TestCase):
         self.assertEqual(rd.splitCommand('setAttr "-k off" foo ".v";select -ne :defaultShaderList1;'), (["setAttr", '"-k off"', "foo", '".v"'],25))
         self.assertEqual(rd.splitCommand('setAttr "foo spam"'), (["setAttr", '"foo spam"'],-1))
         self.assertEqual(rd.splitCommand('setAttr "foo spam'), (["setAttr", '"foo spam"'],-1))
+
+    def testMAReader_getOpt(self):
+        """Test the MAReader.splitCommand() method.
+        """
+
+        rd = mayaascii.MAReader()
+        args,n = rd.splitCommand('setAttr -k off ".v"')
+        self.assertEqual(rd.getOpt(args[1:], rd.setAttr_opt_def, rd.setAttr_name_dict), (['".v"'], dict(keyable=["off"])))
+
+        args,n = rd.splitCommand('setAttr "-k" off ".v"')
+        self.assertEqual(rd.getOpt(args[1:], rd.setAttr_opt_def, rd.setAttr_name_dict), (['".v"'], dict(keyable=["off"])))
+
+        args,n = rd.splitCommand('setAttr ".cuvs" -type "string" "map1";')
+        self.assertEqual(rd.getOpt(args[1:], rd.setAttr_opt_def, rd.setAttr_name_dict), (['".cuvs"', '"map1"'], dict(type=["string"])))
+
+        self.assertEqual(rd.getOpt(["-5"], rd.setAttr_opt_def, rd.setAttr_name_dict), (["-5"], {}))
 
 
 ######################################################################

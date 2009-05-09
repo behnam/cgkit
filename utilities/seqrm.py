ced43020520687300c182e120fd466ec7584df2d
mbaas <mbaas>
2009-05-09 10:19:15 +0000

    seqrm: Ask for confirmation by default before deleting anything.

--- utilities/seqrm.py
+++ utilities/seqrm.py
@@ -41,29 +41,78 @@ import os
 import os.path
 from cgkit import sequence
 
+def promptUser(question):
+    """Print a question and ask for y/n.
+    
+    Returns True when the user entered 'y'.
+    """
+    while 1:
+        print question,
+        answer = raw_input()
+        answer = answer.lower()
+        if answer=="n":
+            return False
+        if answer=="y":
+            return True
+        print "Expected 'y' or 'n'"
 
 def main():
     parser = optparse.OptionParser(usage="%prog [options] sequences")
+    parser.add_option("-f", "--force", action="store_true", default=False, help="Never query the user for confirmation")
     opts,args = parser.parse_args()
 
     if len(args)<1:
         parser.print_usage()
         return
  
+    # Collect all specified sequences...
+    sequences = []
     for pattern in args:
         fseqs = sequence.glob(pattern)
         if len(fseqs)==0:
             print >>sys.stderr, '%s: No sequence "%s" found.'%(os.path.basename(sys.argv[0]), pattern)
             
         for fseq in fseqs:
-            print "Deleting %s"%fseq
-            for fileName in fseq:
-                os.remove(str(fileName))
+            sequences.append(fseq)
+            
+    # No sequences specified? Then there's nothing to do
+    if len(sequences)==0:
+        return 0
+            
+    # Ask for confirmation...
+    if not opts.force:
+        for fseq in sequences:
+            print fseq
+        if len(sequences)==1:
+            msg = "Are you sure to delete the above sequence?"
+        else:
+            msg = "Are you sure to delete the above %s sequences?"%len(sequences)
+        if not promptUser(msg):
+            return 0
+            
+    # Delete the sequences..
+    for fseq in sequences:
+        print "Deleting %s"%fseq
+        for fileName in fseq:
+            os.remove(str(fileName))
             
     return 0
     
 ##########################################################################
 
-ret = main()
+##########################################################################
+
+try:
+    ret = main()
+except SystemExit:
+    ret = 1
+except KeyboardInterrupt:
+    print >>sys.stderr, "\nUser abort"
+    ret = 1
+except:
+    print >>sys.stderr, "ERROR:",sys.exc_info()[1]
+#    raise
+    ret = 1
+
 sys.exit(ret)
 
